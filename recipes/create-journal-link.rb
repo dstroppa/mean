8836f1b10206d37db70528da8943ab7ecb2c946a
Daniele Stroppa <stroppad@amazon.com>
2014-11-11 12:47:54 +0000

    Updated journal fs creation

--- recipes/create-journal-link.rb
+++ recipes/create-journal-link.rb
@@ -1,13 +1,11 @@
-Chef::Log.info("Creating /journal directory with owned by '#{node[:mongodb][:group]}':'#{node[:mongodb][:user]}' ")
-
 directory "/journal" do
-  group node['mongodb']['group']
-  owner node['mongodb']['user']
+  group 'mongod'
+  owner 'mongod'
   mode '0755'
 end
 
 link "/data/journal" do
   to '/journal'
-  group node['mongodb']['group']
-  owner node['mongodb']['user']
+  group 'mongod'
+  owner 'mongod'
 end
