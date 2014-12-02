d076029dbe1306dafe1be9516d5dc5652cc53ffc
Daniele Stroppa <stroppad@amazon.com>
2014-12-02 10:59:22 +0000

    Added MEAN default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,15 @@
+if node[:mongodb][:cluster_name] != nil
+  node.mongodb.cluster_name = node[:mongodb][:cluster_name]
+end
+
+if node[:mongodb][:is_replicaset] != nil
+  node.mongodb.is_replicaset = node[:mongodb][:is_replicaset]
+end
+
+if node[:mongodb][:shard_name] != nil
+  node.mongodb.shard_name = node[:mongodb][:shard_name]
+end
+
 if node[:chef_environment] != nil
   node.chef_environment = node[:chef_environment]
 end
